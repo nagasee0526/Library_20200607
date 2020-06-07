b7825d4faa9492b2ba1ae4a018109d2c06e4b54a
nagasee0526 <roronalove@gmail.com>
2020-06-07 13:46:55 +0900

    TedPermission 라이브러리 전화 연결 권한 설정 라이브러리

--- app/src/main/java/com/ubis/library_20200607/MainActivity.kt
+++ app/src/main/java/com/ubis/library_20200607/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.ubis.library_20200607
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : baseActivity() {
@@ -27,9 +31,27 @@ class MainActivity : baseActivity() {
         }
 
         collBtn.setOnClickListener {
-            val MyUrl = Uri.parse("tel:${PhonenumTxt.text}")
-            val myIntent = Intent(Intent.ACTION_CALL, MyUrl)
-            startActivity(myIntent)
+            // 전화를 걸기전에 권한을 얻었는지 체크
+            // 권한을 허가 받은 경우에만 전화를 검
+            val permissionlistener = object : PermissionListener{
+                override fun onPermissionGranted() { // 허가가 난 경우
+                    // 실제로 전화 거는 고드
+                    val MyUrl = Uri.parse("tel:${PhonenumTxt.text}")
+                    val myIntent = Intent(Intent.ACTION_CALL, MyUrl)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    // 허가가 안난 경우
+                    Toast.makeText(mContext, "권한이 거부되어 통화를 할수없습니다.", Toast.LENGTH_SHORT)
+                }
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("거절되면 통화할수없으니 설정에서 권한을 해제해 주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
         }
     }
 }
